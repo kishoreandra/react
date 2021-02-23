20985a479e3013295d5f3f464fadddcc798a933b
kishoreandra <keechu1008@gmail.com>
2021-02-23 23:25:24 +0530

    state initialization and refer it in JSX

--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import ReactDOM from "react-dom";
 // The class must be a JS class and should extending React.Component parent class
 // The class must have a render method which gets called on creating an App instance
 class App extends React.Component {
+  // A state is a JS object that has data relevant to the component
+  // Updating a state will cause the rerender of component
+  // state must be updated only using setState() method only
+
+  constructor(props) {
+    super(props);
+    this.state = { data: "Its me dude" };
+  }
+
   render() {
-    return <div>Hi there ... I am a class based component</div>;
+    return (
+      <div>
+        {/* refer the value of state as below : */}
+        <h1>Hi there and {this.state.data} ... I am a class based component</h1>
+      </div>
+    );
   }
 }
 
