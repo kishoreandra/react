87086dc1b3d508c4fbe86167768c12e25033755a
kishoreandra <keechu1008@gmail.com>
2021-02-23 22:38:47 +0530

    nested components practice

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-// import libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import ContentDetails from "./ContentDetails";
-// create a react component
+import MovieInfo from "./MovieInfo";
+import Extra from "./Extra";
+
 const App = () => {
   return (
-    <div className="ui feed">
-      {/* send props (propName = propValue) from parent (App) to child (content) like below */}
-      <ContentDetails name="rohith" />
-      <ContentDetails name="dikshith" />
-      <ContentDetails name="sai" />
-      <ContentDetails name="kishore" />
+    <div className="ui divided items">
+      <MovieInfo
+        header="SAAHO"
+        description="A full graphics movie showing many seas around world"
+        name="Sangeetha Movies"
+      />
+      <Extra />
+      <MovieInfo
+        header="UPPENA"
+        description="A romantic love story attracting teenagers and all"
+        name="Inox Movies"
+      />
+      <Extra />
+      <MovieInfo
+        header="KHAIDHI"
+        description="A realistic mass entertainer !!"
+        name="SLV Movies"
+      />
+      <Extra />
     </div>
   );
 };
 
-// render to screen
 ReactDOM.render(<App />, document.querySelector("#root"));
